4155b91365b76096746588c6458fc2854adbc3fd
gbrreel <gabrielggalloo@gmail.com>
2025-11-17 20:33:01 -0300

    Monitor de sites

--- hello.go
+++ hello.go
@@ -4,60 +4,80 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
+const monitoramentos = 2
+const delay = 5
+
 func main() {
-
 	exibeIntroducao()
-
 	for {
 		exibeMenu()
 
-		comando := lerComando()
+		comando := leComando()
 
 		switch comando {
 		case 1:
 			iniciarMonitoramento()
 		case 2:
-			fmt.Println("Exibindo logs...")
+			fmt.Println("Exibindo Logs...")
 		case 0:
-			fmt.Println("Saindo do programa...")
+			fmt.Println("Saindo do programa")
 			os.Exit(0)
 		default:
-			fmt.Println("Comando não reconhecido")
+			fmt.Println("Não conheço este comando")
 			os.Exit(-1)
 		}
 	}
+
 }
 
 func exibeIntroducao() {
-	nome := "Gabriel"
-	fmt.Println("Olá, sr", nome)
+	nome := "Douglas"
+	versao := 1.2
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
 }
 
 func exibeMenu() {
-	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibir logs")
-	fmt.Println("0 - Sair do programa")
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
 }
 
-func lerComando() int {
+func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
-	fmt.Println("Comando escolhido foi", comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 
 	return comandoLido
 }
 
 func iniciarMonitoramento() {
-	fmt.Println("Iniciando monitoramento...")
-	site := "https://www.redbull.com/br-pt/tags/f1"
+	fmt.Println("Monitorando...")
+
+	sites := []string{"https://random-status-code.herokuapp.com/", "https://www.alura.com.br", "https://www.caelum.com.br"}
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+
+	fmt.Println("")
+}
+
+func testaSite(site string) {
 	resp, _ := http.Get(site)
 
 	if resp.StatusCode == 200 {
-		fmt.Println("Site", site, "foi carregado com sucesso!")
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
 	} else {
-		fmt.Println("Site", site, "está com problemas. Status code:", resp.StatusCode)
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
 	}
-
 }
